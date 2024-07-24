dcd1c733f0c54375e981c2140ad283437b5b19b0
Alberth Condori <alberth.condori@fundacion-jala.org>
2024-07-24 03:27:23 -0400

    initial commit

---
+++ OrderProcessing/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderProcessing;
+
+public class Order
+{
+  public Customer Customer { get; set; }
+  
+  public List<string> Items { get; set; }
+  
+  public double TotalAmount { get; set; }
+  
+  public string PaymentStatus { get; set; }
+}
