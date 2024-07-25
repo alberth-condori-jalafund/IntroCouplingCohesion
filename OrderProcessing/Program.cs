2e614143e296622555a31d61e6ef9d87ecb7aa2b
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2024-07-24 20:42:56 -0400

    refactor: separating the responsibilities of the ProcessOrder method

--- OrderProcessing/Program.cs
+++ OrderProcessing/Program.cs
@@ -2,11 +2,11 @@
 
 public class Program
 {
-  public static void Main()
-  {
-    var customer = new Customer ("Alberth Condori", "alberth.condori@example.com", "123 Main St");
-    var items = new List<(string, int)> { ("Item1", 2), ("Item2", 1), ("Item3", 1) };
-    var orderService = new OrderService();
-    orderService.ProcessOrder(customer, items);
-  }
+    public static void Main()
+    {
+        var customer = new Customer("Alberth Condori", "alberth.condori@example.com", "123 Main St");
+        var items = new List<(string, int)> { ("Item1", 2), ("Item2", 1), ("Item3", 1) };
+        var orderService = new OrderService();
+        orderService.ProcessOrder(customer, items);
+    }
 }
