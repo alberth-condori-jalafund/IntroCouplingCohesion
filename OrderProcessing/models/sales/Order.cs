e9ab3bb7c4bf5667f0b43258f1aed93ce251ff4e
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-07-24 22:50:55 -0400

    refactor: reordering the structure of sales models

---
+++ OrderProcessing/models/sales/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderProcessing;
+
+public class Order<T> where T : AbstractItem
+{
+  public Customer Customer { get; set; }
+  public List<T> Items { get; set; }
+  public double TotalAmount { get; set; }
+  public PaymentStatus PaymentStatus { get; set; }
+}
