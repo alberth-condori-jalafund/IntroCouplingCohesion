7c90a1bbd0a46bd9b4d4a9c721a00ee2f378adc8
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2024-07-25 03:25:41 -0400

    refactor

--- OrderProcessing/Program.cs
+++ OrderProcessing/Program.cs
@@ -4,9 +4,22 @@ public class Program
 {
   public static void Main()
   {
-    var customer = new Customer { Name = "Alberth Condori", Email = "alberth.condori@example.com", Address = "123 Main St" };
-    var items = new List<(string, int)> { ("Item1", 2), ("Item2", 1), ("Item3", 1) };
-    var orderService = new OrderService();
-    orderService.ProcessOrder(customer, items);
+    var inventory = new Inventory();
+
+    var item1 = new Item(1, "Item1", 10);
+    var item2 = new Item(2, "Item2", 5);
+    var item3 = new Item(3, "Item3", 8);
+
+    inventory.AddItem(item1, 10);
+    inventory.AddItem(item2, 10);
+    inventory.AddItem(item3, 10);
+
+    var customer = new Customer ("Alberth Condori", "alberth.condori@example.com", "123 Main St");
+
+    var orderService = new OrderService(inventory);
+
+
+    var customerItems = new List<(Item, int)> { (item1, 5), (item2, 5), (item3, 5)};
+    orderService.ProcessOrder(customer, customerItems);
   }
 }
--- OrderProcessing/models/Customer.cs
+++ OrderProcessing/models/Customer.cs
@@ -7,4 +7,11 @@ public class Customer
   public string Email { get; set; }
   
   public string Address { get; set; }
+
+  public Customer( string name, string email, string address )
+  {
+    Name = name;
+    Email = email;
+    Address = address;
+  }
 }
